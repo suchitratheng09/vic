06438b053b76f5b0842f90e105aacdace09f0de5
suchitra <suchitratheng09@gmail.com>
2020-01-05 10:35:17 +0530

    vichitra

---
+++ first_program.py
@@ -0,0 +1,22 @@
+import os
+import math
+import sys
+# Assigning Value To variable
+# a = 10
+
+a =int (sys.argv[1])
+b = int (sys.argv[2])
+print (sys.argv)
+name = "ABC"
+def my_function(x, y):
+'''
+      This Function returns Addition
+'''
+return x + y
+	
+ans = my_function (a,b)
+
+"""
+Can Add Multiple Commnets Here
+"""
+print(ans)
